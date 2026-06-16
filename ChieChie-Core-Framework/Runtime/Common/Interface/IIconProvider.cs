44bff94054e2c9d27d0db80653f39424eda1b689
chiendm1 <chiendm@sonat.vn>
2026-06-16 11:33:52 +0700

    ok

--- ChieChie-Core-Framework/Runtime/Common/Interface/IIconProvider.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ChieChie.Core
-{
-    public interface IIconProvider
-    {
-        Sprite GetRewardIcon(ResourceType type, bool isInfinite);
-    }
-}
--- ChieChie-Core-Framework/Runtime/Core/SDK Bridge/IRemoteConfigBrigde.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ChieChie.Core
-{
-    public interface IRemoteConfigBrigde
-    {
-     
-    }
-}
