d4c610207cf13ff8d5a8d4db7a537bdb2ae314de
DESKTOP-1K6JUT0\Minh Chien <minhchien11014481@gmail.com>
2026-07-03 05:12:34 +0700

    ok

--- PassDatabase.cs
+++ PassDatabase.cs
@@ -10,11 +10,11 @@ namespace ChieChie.GamePass
         [SerializeField] GamePassSettings gamePassSettings;
         [SerializeField] private List<PassData> passItems = new List<PassData>();
         [SerializeField] private List<PassBonusData > bonusPassItems = new List<PassBonusData>();
-        [SerializeField] private List<PassReplaceReward> bonusPassBonusItems = new List<PassReplaceReward>();
+        [SerializeField] private List<PassReplaceItem> replacePassItems = new List<PassReplaceItem>();
 
         public IReadOnlyList<PassData> PassItems => passItems;
         public IReadOnlyList<PassBonusData> BonusPassItems => bonusPassItems;
-        public IReadOnlyList<PassReplaceReward> BonusPassBonusItems => bonusPassBonusItems;
+        public IReadOnlyList<PassReplaceItem> ReplacePassItems => replacePassItems;
         public GamePassSettings GamePassSettings => gamePassSettings;
     }
 
