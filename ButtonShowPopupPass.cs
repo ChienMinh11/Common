9c90fc6c8defb162a1f4803be121c2b6f7cb11c3
ChienMinh11 <128205692+ChienMinh11@users.noreply.github.com>
2026-07-04 09:48:27 +0700

    Add no-fade option to pass popup button

--- ButtonShowPopupPass.cs
+++ ButtonShowPopupPass.cs
@@ -6,16 +6,26 @@ namespace Game.GamePlay
 {
     public class ButtonShowPopupPass : MonoBehaviour
     {
+        [SerializeField] private bool noFade;
+
         private IPopupService _popupService;
+
+        public bool NoFade => noFade;
+
         [Inject]
         private void Contructor(IPopupService popupService)
         {
             _popupService = popupService;
         }
 
+        public void SetNoFade(bool value)
+        {
+            noFade = value;
+        }
+
         public void OnShowPopup()
         {
-            _popupService.ShowPopup("PopupGamePass");
+            _popupService.ShowPopup("PopupGamePass", noFade);
         }
     }
 }
